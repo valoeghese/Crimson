9bb4c0d8ef41842536baf10049e05dd320978ef0
valoeghese <valoeghesemods@gmail.com>
2020-03-30 16:37:53 +1300

    start interaction logic

---
+++ src/main/java/modfest/teamgreen/magic/MagicInteraction.java
@@ -0,0 +1,13 @@
+package modfest.teamgreen.magic;
+
+import modfest.teamgreen.magic.attribute.Attribute;
+
+public class MagicInteraction {
+	//           device[0]  device[1]  device[2]
+	// start ->  device[3]  device[4]  device[5]
+	//           device[6]  device[7]  device[8]
+	public MagicInteraction(Attribute start, Attribute[] device) {
+		// construct device interaction here
+		// save constructed device
+	}
+}
