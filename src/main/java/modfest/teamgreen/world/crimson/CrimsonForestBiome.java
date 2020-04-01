2bc9c2099c149af80e4bd8305980539506d86a73
Waterpicker <waterpickerenternity@gmail.com>
2020-04-01 04:52:44 -0500

    Forgot leaves

--- src/main/java/modfest/teamgreen/world/crimson/CrimsonForestBiome.java
+++ src/main/java/modfest/teamgreen/world/crimson/CrimsonForestBiome.java
@@ -1,6 +1,8 @@
 package modfest.teamgreen.world.crimson;
 
+import modfest.teamgreen.world.Features;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStep;
 
 public class CrimsonForestBiome extends CrimsonBaseBiome {
 	public CrimsonForestBiome() {
@@ -8,5 +10,6 @@ public class CrimsonForestBiome extends CrimsonBaseBiome {
 				.category(Biome.Category.FOREST)
 				.depth(0.2f)
 				.scale(0.38f));
+		this.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, Features.CRIMSON_TREE_FEATURE);
 	}
 }
